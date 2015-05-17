cf1aaaa6e6a67a164da9af2927fdac54f7854c18
Oleksandr Lavrushchenko <kpykcb@gmail.com>
2015-05-17 17:11:54 +0200

    upd: video convesion is updated, classified, almost ready to include in main script

--- src/video2bag.py
+++ src/video2bag.py
@@ -17,13 +17,38 @@ from cv_bridge import CvBridge, CvBridgeError
 
 topic = 'camera/image_raw'
 
-def convert(capture):
-	with rosbag.Bag('output_video.bag', 'w') as outbag:
+try:
+	import rospy
+	import rosbag
+	from apmlog_tools.msg import AHR2
+except ImportError:
+	print "Can't load ROS dependencies"
+
+
+class CameraHandler:
+	'''Handle camera data'''
+	def __init__(self):
+		name = 'none'
+		self.showFrames = False
+
+	def setVerbose(self, showFrames):
+		self.showFrames = showFrames
+
+	def setName(self, name):
+		self.name = name
+		self.capture = cv2.VideoCapture(self.name)
+
+		if self.capture.isOpened() == True:
+			print("Input file is opened. Ready to perform convertion.")
+		else:
+			print("ERROR: Unable to open given video file!")
+
+	def convertData(self, bagfile):
 		seq = 0
 		bridge = CvBridge()
 		while(True):
 			# Capture frame-by-frame
-			ret, cvImage = capture.read()
+			ret, cvImage = self.capture.read()
 			try:
 				imageMsg = bridge.cv2_to_imgmsg(cvImage, "bgr8") # TODO: format spec as cmd option?
 			except CvBridgeError, e:
@@ -37,22 +62,23 @@ def convert(capture):
 			imageMsg.header.stamp =  rospy.Time.from_sec(time.time()) 
 
 			# write message to bag file
-			outbag.write(topic, imageMsg, imageMsg.header.stamp)
+			bagfile.write(topic, imageMsg, imageMsg.header.stamp)
 
 			# this is not so important for conversion
-			cv2.imshow('frame', cvImage)
-			if cv2.waitKey(1) & 0xFF == ord('q'):
-				break
+			if self.showFrames == True:
+				cv2.imshow('frame', cvImage)
+				if cv2.waitKey(1) & 0xFF == ord('q'):
+					break
 
-
-
-	# Our operations on the frame come here
-	# gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	# # Display the resulting frame
-	# cv2.imshow('frame',gray)
-	# if cv2.waitKey(1) & 0xFF == ord('q'):
-	# 	break
-
+		# Our operations on the frame come here
+		# gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		# # Display the resulting frame
+		# cv2.imshow('frame',gray)
+		# if cv2.waitKey(1) & 0xFF == ord('q'):
+		# 	break
+		# When everything done, release the capture
+		self.capture.release()
+		cv2.destroyAllWindows()
 
 def main():
 
@@ -61,16 +87,21 @@ def main():
 	parser.add_argument('-s', '--sync_time', metavar='', action='store_const', const=True, help='sync video to logfile') # TODO: add sync time code
 	args = parser.parse_args()
 
-	cap = cv2.VideoCapture(args.logfile.name)
+	
 
-	if cap.isOpened() == True:
-		convert(cap)
+	cam = CameraHandler()
 
-	# When everything done, release the capture
-	cap.release()
-	cv2.destroyAllWindows()
+	# starting conversion
+	bag = rosbag.Bag('output_video.bag', 'w')
 
-	print()
+	cam.setName(args.logfile.name)
+	cam.setVerbose(False)
+	print("Converting...")
+	cam.convertData(bag)
+
+	# handlers finished
+	bag.close()
+	print("Convesion finished.")
 
 
 if __name__ == "__main__":
